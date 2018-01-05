dbe99862de43fedadb3855a2507a2a908efcebe7
zhoumingque <mail@zhoumq.cn>
2018-01-05 21:14:43 +0800

    添加utils工具函数

--- src/utils/enumerables.ts
+++ src/utils/enumerables.ts
@@ -1,8 +1,9 @@
 
 interface StringValidator {
     isPass(s: string): boolean;
-
 }
+
+import * as _ from 'lodash';
 
 const lettersRegexp = /^[A-Za-z]+$/;
 const numberRegexp = /^[0-9]+$/;
@@ -46,9 +47,9 @@ export function decorateArmour(target, key, descriptor) {
  * @param propName 属性
  * @param pattern 运算符
  */
-export function sortByProp(propName, pattern = '>') {
+export function sortByProp(propName: string, pattern = '>') {
     let sortFunc: any;
-    sortFunc =  (obj1, obj2) => {
+    sortFunc = (obj1, obj2) => {
         if (eval(`${obj1[propName]} ${pattern} ${obj2[propName]}`)) {
             return 1;
         } else if (obj1[propName] === obj2[propName]) {
@@ -64,8 +65,37 @@ export function sortByProp(propName, pattern = '>') {
  * 生成判断对象类型的高阶函数
  * @param type 对象类型
  */
-export function isType(type){
- return function(obj){
-   return Object.prototype.toString.call(obj) == `[object ${type}]`;
- };
+export function isType(type: string) {
+    return function(obj) {
+        return Object.prototype.toString.call(obj) == `[object ${type}]`;
+    };
+}
+
+/**
+ * 获取元素在数组中的索引数组
+ * @param arr 数组
+ * @param num 数量
+ * @param the 查看的元素
+ */
+export function indexArray(arr: any[], num: number, the: any) {
+    const indexArr: any[] = [];
+    let index = -1;
+    for (let i = 0; i < num; i++) {
+        index = _.indexOf(arr, the, index + 1);
+        indexArr.push(index);
+    }
+    return indexArr;
+}
+
+/**
+ * 查看数组中元素个数
+ * @param {数组} array
+ * @param {元素} the
+ */
+export function numberByArray(array: any[], the: any) {
+    let c = _.cloneDeep(array);
+    c = c.filter((element, index, array) => {
+        return (element == the);
+    });
+    return c.length;
 }

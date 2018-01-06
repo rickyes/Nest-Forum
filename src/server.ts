1ddc906a7f3ce4bd75fb7f06f3eede9a36d8208a
泽阳Allen <mail@zhoumq.cn>
2018-01-06 23:29:49 +0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/app.module';
 
+/**
+* app listen
+**/
 async function bootstrap() {
     const app = await NestFactory.create(ApplicationModule);
     await app.listen(3000);
